c3535e41ec41aa711d50156d912db8344566e5bd
Your New Name <your.email@example.com>
2025-03-25 22:40:39 +0800

    Updated black and orange theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center bg-light p-5 rounded">
+    <div class="jumbotron text-center bg-dark text-white p-5 rounded">
         <h1 class="display-4">College Department System</h1>
     </div>
 
     <div class="row mt-5">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-header bg-primary text-white">
+            <div class="card border-dark shadow-sm">
+                <div class="card-header bg-gradient-warning text-black">
                     <h4>Colleges</h4>
                 </div>
-                <div class="card-body">
+                <div class="card-body bg-dark text-white">
                     <p>Manage all colleges in the system:</p>
                     <ul>
                         <!-- <li>Create new colleges</li>
@@ -19,16 +19,16 @@
                         <li>View college details</li>
                         <li>Delete colleges (soft deletion)</li> -->
                     </ul>
-                    <a href="{{ route('colleges.index') }}" class="btn btn-outline-primary">Go to Colleges</a>
+                    <a href="{{ route('colleges.index') }}" class="btn btn-warning text-black">Go to Colleges</a>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-header bg-success text-white">
+            <div class="card border-dark shadow-sm">
+                <div class="card-header bg-gradient-warning text-black">
                     <h4>Departments</h4>
                 </div>
-                <div class="card-body">
+                <div class="card-body bg-dark text-white">
                     <p>Manage all departments in the system:</p>
                     <ul>
                         <!-- <li>Create new departments</li>
@@ -36,7 +36,7 @@
                         <li>View department details</li>
                         <li>Delete departments (soft deletion)</li> -->
                     </ul>
-                    <a href="{{ route('departments.index') }}" class="btn btn-outline-success">Go to Departments</a>
+                    <a href="{{ route('departments.index') }}" class="btn btn-warning text-black">Go to Departments</a>
                 </div>
             </div>
         </div>
